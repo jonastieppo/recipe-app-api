64ed24d6ffcdf36f9b1faba8c7aacae1669b4055
Jonas Tieppo <jonastieppo@yahoo.com>
2024-12-25 23:30:14 -0300

    adding postgress

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """ Add x and y and return the result """
+    return x + y
+
+
+def subtract(x,y):
+    """ Substract x and y and return the result """
+    return x - y
