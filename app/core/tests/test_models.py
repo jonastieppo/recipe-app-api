67fb5155dc0abfcb0108949830d731848bbb3e71
Jonas Tieppo <jonastieppo@yahoo.com>
2025-01-05 18:16:17 -0300

    create subdirections to handle media and migrations

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,6 +6,7 @@ from decimal import Decimal
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from unittest.mock import patch
 
 from core import models
 
@@ -89,3 +90,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    @patch('core.models.build.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating image path"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
